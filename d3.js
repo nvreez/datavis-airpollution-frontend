6a4f7492db6c7be8289b2af930ea0e41a1b7d3f5
Nathan de Vries <nathan@nathandevri.es>
2017-06-13 16:02:15 +0800

    Now uses API

--- d3.js
+++ d3.js
@@ -1,5 +1,6 @@
-var locations = ['central_western', 'eastern', 'kwun_tong', 'sham_shui_po', 'kwai_chung', 'tsuen_wan', 'tseung_kwan_o', 'yuen_long', 'tuen_mun', 'tung_chung', 'tai_po', 'shatin', 'tap_mun', 'causeway_bay', 'central', 'mong_kok'];
+var locs = ['centralWestern', 'eastern', 'kwunTong', 'shamShuiPo', 'kwaiChung', 'tsuenWan', 'tseungKwanO', 'yuenLong', 'tuenMun', 'tungChung', 'taiPo', 'shatin', 'tapMun', 'causewayBay', 'central', 'mongKok'];
 var colors = ["#83E827", "#3DBA32", "#03943B", "#FFD91B", "#FFA51C", "#FC7D1F", "#E91B33", "#B6005C", "#88007D", "#63008C", "#5D0021"];
+
 
 var margin = { top: 20, right: 20, bottom: 30, left: 50 },
     width = 1280 - margin.left - margin.right,
@@ -8,13 +9,16 @@ var margin = { top: 20, right: 20, bottom: 30, left: 50 },
 var n = 24; // n-period of moving average
 var k = 2;  // k times n-period standard deviation above/below moving average
 
-// var parseDate = d3.time.format("%m/%d/%Y").parse;
+var loc = "tungChung";
+var api = "http://127.0.0.1:8088/api/";
 
 var x = d3.scaleTime()
         .range([0, width]);
 var y = d3.scaleLinear()
         .domain([0, 11])
         .range([height, 0]);
+var colorScale = d3.scaleLinear()
+    .range(colors);
 
 
 var xAxis = d3.axisBottom()
@@ -26,10 +30,10 @@ var yAxis = d3.axisLeft()
 
 var line = d3.line()
         .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.central); });
-var ma = d3.line()
+        .y(function(d) { return y(d[loc]); });
+var mean = d3.line()
         .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.ma); });
+        .y(function(d) { return y(d.mean); });
 var lowBand = d3.line()
         .x(function(d) { return x(d.date); })
         .y(function(d) { return y(d.low); });
@@ -47,7 +51,8 @@ var svg = d3.select(".chart")
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("201705_Eng_clean.csv", type, function(error, data) {
+d3.json(api + "pollutionRecords?from=20161101&to=20161130", function(error, data) {
+    data = type(data);
     var bandsData = getBollingerBands(n, k, data);
 
     x.domain(d3.extent(data, function(d) { return d.date; }));
@@ -75,8 +80,8 @@ d3.csv("201705_Eng_clean.csv", type, function(error, data) {
 
     svg.append("path")
             .datum(bandsData)
-            .attr("class", "line ma bands")
-            .attr("d", ma);
+            .attr("class", "line mean bands")
+            .attr("d", mean);
 
     // Add the scatterplot
     svg.selectAll("circle")
@@ -84,52 +89,44 @@ d3.csv("201705_Eng_clean.csv", type, function(error, data) {
       .enter().append("circle")
         .attr("class", "scatterplot dot")
         .attr("r", 1)
+        // .style("fill", function(d) {
+        //   return colors[d[loc] - 1]; })
         .attr("cx", function(d) {
           return x(d.date); })
-        .attr("cy", function(d) { return y(d.central); });
+        .attr("cy", function(d) { return y(d[loc]); });
 });
 
 
 function getBollingerBands(n, k, data) {
   var bands = []; //{ ma: 0, low: 0, high: 0 }
+  console.log("d3.js:101", data);
   for (var i = 1, len = data.length; i < len; i++) {
     var slice = data.slice(Math.max(i + 1 - n, 0), i);
-    var mean = d3.mean(slice, function(d) { return d.central; });
-    var min = d3.min(slice, function(d) { return d.central; });
-    var max = d3.max(slice, function(d) { return d.central; });
-    var qlow = d3.quantile(slice, 0.25, function(d) { return d.central; });
-    var qhigh = d3.quantile(slice, 0.75, function(d) { return d.central; });
-    var avHigh = d3.mean(slice.filter(function(d) {
-      return d.central > mean;
-    }), function(d) { return d.central; }) || mean;
-    var avLow = d3.mean(slice.filter(function(d) {
-      return d.central < mean;
-    }), function(d) { return d.central; }) || mean;
+    var mean = d3.mean(slice, function(d) { return d[loc]; });
+    var min = d3.min(slice, function(d) { return d[loc]; });
+    var max = d3.max(slice, function(d) { return d[loc]; });
     var stdDev = Math.sqrt(d3.mean(slice.map(function(d) {
-      return Math.pow(d.central - mean, 2);
+      return Math.pow(d[loc] - mean, 2);
     })));
     bands.push({ date: data[i].date,
-     ma: mean,
+     mean: mean,
      min: min,
      max: max,
-     // low: qlow,
-     // high: qhigh});
-     // low: avLow,
-     // high: avHigh});
      low: mean - (k * stdDev),
      high: mean + (k * stdDev) });
   }
+  console.log("d3.js:114", bands);
   return bands;
 }
 
 
 function type(d) {
-  for (var i = locations.length - 1; i >= 0; i--) {
-    d[locations[i]] = +d[locations[i]];
+  for (var i = d.length - 1; i >= 0; i--) {
+    var hour = ("0" + d[i].hour).slice(-2);
+    d[i].date = new Date(d[i].date + "T" + hour + ":00:00");
   }
-  d.date = new Date(d.rdate + "T" + d.rhour + ":00:00");
-  d.rhour = +d.rhour;
-  d.rdate = new Date(d.rdate);
+  // d.rhour = +d.rhour;
+  // d.date = new Date(d.date);
   return d;
 }
 
