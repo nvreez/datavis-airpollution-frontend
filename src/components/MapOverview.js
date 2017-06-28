d45427271f7d5c46787cb67d842ee72d92a093a5
Nathan de Vries <nathan@nathandevri.es>
2017-06-28 20:53:50 +0800

    Overlay data using coloured voronoi

--- src/components/MapOverview.js
+++ src/components/MapOverview.js
@@ -13,20 +13,46 @@ class Intro extends Component {
         height: 1100,
       },
       context: {},
+      data: locations
     };
     this.radius = .3;
     this.onImgLoad = this.onImgLoad.bind(this);
+    this.colorScale = d3.scaleLinear()
+      .domain([0, 7, 11])
+      .range(["#35B9FF", "#8C7E5E", "#532C13"])
+      .interpolate(d3.interpolateHcl)
   }
 
   componentDidMount() {
     this.context = d3.select(this.chart);
-    this.context.selectAll('circle')
-      .style('fill', 'red');
+    this.voronoi = this.createVoronoi();
+
+
+    d3.json("http://127.0.0.1:8088/api/pollution-records?from=20170401&to=20170401&granularity=24", (error, data) => {
+      data = this.dateType(data[0]);
+
+      this.setState({data});
+      this.updatePolygons(data);
+    });
+  }
+
+
+  dateType(d) {
+    return locations.map(location => {
+      return Object.assign(d[location.location], location);
+    });
   }
 
   setViewbox() {
     this.context.select('svg')
       .attr("viewBox", `0 0 ${this.state.dimensions.width} ${this.state.dimensions.height}`);
+  }
+
+  createVoronoi() {
+    return d3.voronoi()
+      .extent([[-1,-1], [this.state.dimensions.width, this.state.dimensions.height]])
+      .x(d => { return d.cx * this.state.dimensions.width; })
+      .y(d => { return d.cy * this.state.dimensions.height; });
   }
 
   createPoints() {
@@ -60,6 +86,35 @@ class Intro extends Component {
         });
   }
 
+  createPolygons(data) {
+    return this.context.append("g")
+        .attr("class", "polygons")
+      .selectAll("path")
+      .data(this.voronoi.polygons(data))
+      .enter().append("path")
+      .attr("d", function(d) { return d ? "M" + d.join("L") + "Z" : null; })
+      .style("fill", d => { return this.colorScale(d.data.mean || 0); })
+      .style("opacity", d => { return d.data.mean / 12 || 0; });
+  }
+
+  updatePolygons(data) {
+    return this.context.select(".polygons")
+      .selectAll("path")
+      .data(this.voronoi.polygons(data))
+      .transition()
+      .style("fill", d => { return this.colorScale(d.data.mean); })
+      .style("opacity", d => {
+        console.log("MapOverview.js:107", d.data.mean / 20 );
+        return d.data.mean / 20; });
+  }
+
+  colorPolygon(polygon) {
+    console.log("MapOverview.js:107", this);
+    polygon.style("fill", d => {
+          return this.colorScale(d.data.mean);
+        });
+  }
+
   onImgLoad({target:img}) {
     this.setState({
       dimensions: {
@@ -68,6 +123,8 @@ class Intro extends Component {
       }
     }, function(){
       this.setViewbox();
+      this.voronoi = this.createVoronoi();
+      this.createPolygons(this.state.data);
       this.createPoints();
       this.createLabels();
     });
